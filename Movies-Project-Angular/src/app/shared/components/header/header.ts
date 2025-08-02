cf36892017f94b41e79d3d217105efc6a8badb93
Milen03 <milen.atanasovv03@gmail.com>
2025-08-02 19:52:30 +0300

    Add header nav html and css

--- Movies-Project-Angular/src/app/shared/components/header/header.ts
+++ Movies-Project-Angular/src/app/shared/components/header/header.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './header.css'
 })
 export class Header {
+  protected authService = inject(AuthService); 
+  private router = inject(Router);
+
+  readonly isLoggedIn = this.authService.isLoggedIn;
+  readonly currentUser = this.authService.currentUser;
+
+
+    
 
 }
