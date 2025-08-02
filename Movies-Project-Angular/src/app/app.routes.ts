2d452c5c025093b29abd792a09d0436cf300bd17
Milen03 <milen.atanasovv03@gmail.com>
2025-08-02 20:34:12 +0300

    Upadate router Add router for login register and logout

--- Movies-Project-Angular/src/app/app.routes.ts
+++ Movies-Project-Angular/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import path from 'path';
 
 export const routes: Routes = [
     {
@@ -11,10 +10,18 @@ export const routes: Routes = [
         path: 'home',
         loadComponent: () => import('./features/home/home').then(c => c.Home)
     },
-    // {
-    // path: 'logout',
-    // redirectTo: '/home',
-    // pathMatch: 'full'
-    // },
-   
+    {
+        path: 'login',
+        loadComponent: () => import('./features/auth/login/login').then(c => c.Login)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./features/auth/register/register').then(c => c.Register)
+    },
+    {
+         path: 'logout',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
+
 ];
