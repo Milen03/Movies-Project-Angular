be28b309d718f31e626795173507f1bf5a3f05f1
Milen03 <milen.atanasovv03@gmail.com>
2025-08-03 18:09:48 +0300

    Add logn to work

--- Movies-Project-Angular/src/app/features/auth/login/login.ts
+++ Movies-Project-Angular/src/app/features/auth/login/login.ts
@@ -1,12 +1,104 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/auth.service';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private formBuilder = inject(FormBuilder)
 
+  loginForm: FormGroup
+
+  constructor() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(/^(?=.{6,})[a-zA-Z][a-zA-Z0-9._-]*@gmail\.(com|bg)$/)]],
+      password: ['', [Validators.required, Validators.minLength(5), Validators.pattern(/^[a-zA-Z0-9]+$/)]]
+    })
+  }
+
+  get email(): AbstractControl<any, any> | null {
+    return this.loginForm.get('email');
+  }
+
+  get password(): AbstractControl<any, any> | null {
+    return this.loginForm.get('password');
+  }
+
+  get isEmailValid(): boolean {
+    return this.email?.invalid && (this.email?.dirty || this.email?.touched) || false;
+  }
+
+  get isPasswordValid(): boolean {
+    return this.password?.invalid && (this.password?.dirty || this.password?.touched) || false;
+  }
+
+  get emailErrorMessage(): string {
+    if (this.email?.errors?.['required']) {
+      return 'Email is required!';
+    }
+
+    if (this.email?.errors?.['pattern']) {
+      return 'Email is not valid!';
+    }
+
+    return '';
+  }
+
+  get passwordErrorMessage(): string {
+    if (this.password?.errors?.['required']) {
+      return 'Password is required!';
+    }
+
+    if (this.password?.errors?.['minlength']) {
+      return 'Password must be at least 5 characters!';
+    }
+
+    return '';
+  }
+
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+
+      this.authService.login(email, password).subscribe({
+        next: () => {
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          console.error('Login failed', err);
+          this.markFormGroupTouched();
+        }
+      })
+    }
+  }
+
+  private markFormGroupTouched(): void {
+    Object.keys(this.loginForm.controls).forEach(key => {
+      const control = this.loginForm.get(key);
+      control?.markAsTouched();
+    })
+  }
 }
+
+
+export function emailValidator(emailControl: AbstractControl): ValidationErrors | null {
+  const emailRegex = /^(?=.{6,})[a-zA-Z][a-zA-Z0-9._-]*@gmail\.(com|bg)$/;
+
+  const email = emailControl.value;
+
+  if (email && !emailRegex.test(email)) {
+    return { emailValidator: true }
+  }
+
+  return null;
+}
+
+
