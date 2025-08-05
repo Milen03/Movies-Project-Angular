b66ca1610939bc3ab5e0f79bde7eaa2c8b08c558
Milen03 <milen.atanasovv03@gmail.com>
2025-08-05 19:30:57 +0300

    Create movies.service.ts

---
+++ Movies-Project-Angular/src/app/core/service/movies.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Movie } from "../../models/movies.model";
+
+@Injectable({ providedIn: 'root' })
+export class MoviesService {
+  private apiUrl = 'http://localhost:3000/api/movie';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getAll(): Observable<Movie[]> {
+    return this.httpClient.get<Movie[]>(this.apiUrl);
+  }
+
+  getById(id: string): Observable<Movie> {
+    return this.httpClient.get<Movie>(`${this.apiUrl}/${id}`);
+  }
+
+  create(movie: Partial<Movie>): Observable<Movie> {
+    return this.httpClient.post<Movie>(this.apiUrl, movie);
+  }
+
+  update(id: string, movie: Partial<Movie>): Observable<Movie> {
+    return this.httpClient.put<Movie>(`${this.apiUrl}/${id}`, movie);
+  }
+
+  delete(id: string): Observable<any> {
+    return this.httpClient.delete(`${this.apiUrl}/${id}`);
+  }
+
+  like(id: string): Observable<any> {
+    return this.httpClient.put(`${this.apiUrl}/${id}/like`, {});
+  }
+}
