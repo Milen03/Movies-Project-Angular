e21a2a46f22804358e538c166eb969d725aac6dd
Milen03 <milen.atanasovv03@gmail.com>
2025-08-05 18:52:29 +0300

    Add model for movies

---
+++ Movies-Project-Angular/src/app/models/movies.model.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+  _id: string;
+  title: string;
+  genre: string;
+  releaseDate: Date | string;
+  description: string;
+  imageUrl: string;
+  createdBy: string;
+  likes?: string[];
+}
--- Rest-api/Rest-api/models/postModel.js
+++ Rest-api/Rest-api/models/postModel.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema.Types;
 
 const postSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: true
-    },
+
     title: {
         type: String,
         required: true
@@ -34,10 +31,7 @@ const postSchema = new mongoose.Schema({
         type: ObjectId,
         ref: "User"
     },
-    themeId: {
-        type: ObjectId,
-        ref: "Theme"
-    },
+
 }, { timestamps: { createdAt: 'created_at' } });
 
 module.exports = mongoose.model('Post', postSchema);
