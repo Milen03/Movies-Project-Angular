90dec238f38094ee6d47ff1b000e19029247b609
Milen03 <milen.atanasovv03@gmail.com>
2025-08-06 21:56:23 +0300

    Update movies.ts

--- Movies-Project-Angular/src/app/features/movies/movies/movies.ts
+++ Movies-Project-Angular/src/app/features/movies/movies/movies.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/service';
+import { Observable } from 'rxjs';
+import { MoviesService } from '../../../core/service/movies.service';
+import { Movie } from '../../../models/movies.model';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movies',
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './movies.html',
   styleUrl: './movies.css'
 })
 export class Movies {
 
+  private authService = inject(AuthService)
+  readonly isLoggedIn = this.authService.isLoggedIn
+
+  movies$ : Observable<Movie[]>;
+
+  constructor(private moviesService: MoviesService){
+    
+    this.movies$ = this.moviesService.movies$;
+
+    this.moviesService.getAll().subscribe()
+  }
 }
