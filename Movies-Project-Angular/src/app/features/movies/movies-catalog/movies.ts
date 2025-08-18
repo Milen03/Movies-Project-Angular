8c91aca4d90603c43d5d4766d1d05afbf93c5e0c
Milen03 <milen.atanasovv03@gmail.com>
2025-08-18 21:27:38 +0300

    Make filter for Genre

--- Movies-Project-Angular/src/app/features/movies/movies-catalog/movies.ts
+++ Movies-Project-Angular/src/app/features/movies/movies-catalog/movies.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../core/service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
 import { MoviesService } from '../../../core/service/movies.service';
 import { Movie } from '../../../models/movies.model';
 import { CommonModule } from '@angular/common';
@@ -8,25 +8,55 @@ import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movies',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './movies.html',
   styleUrl: './movies.css'
 })
 export class Movies {
 
   private authService = inject(AuthService)
-  private router = inject(Router);  
+  private router = inject(Router);
+  private moviesService = inject(MoviesService);
+
   readonly isLoggedIn = this.authService.isLoggedIn
 
-  movies$ : Observable<Movie[]>;
+  private selectedGenre = new BehaviorSubject<string>('All')
 
-  constructor(private moviesService: MoviesService){
-    
-    this.movies$ = this.moviesService.movies$;
 
-    this.moviesService.getAll().subscribe()
+  movies$!: Observable<Movie[]>;
+  genres$!: Observable<string[]>;
+
+
+  ngOnInit() {
+    const allMovies$ = this.moviesService.movies$;
+
+    this.genres$ = allMovies$.pipe(
+      map(movies => [...new Set(movies.map(m => m.genre))])
+    )
+
+    //  Модифицираме съществуващия `movies$` да филтрира
+    this.movies$ = combineLatest([
+      allMovies$,
+      this.selectedGenre.asObservable()
+    ]).pipe(
+      map(([movies, selectedGenre]) => {
+        if (selectedGenre === 'All') {
+          return movies;
+        }
+        return movies.filter(movie => movie.genre === selectedGenre);
+      })
+    );
+
+    //  Зареждаме данните от API-то
+    this.moviesService.getAll().subscribe();
   }
 
+   onGenreChange(event: Event): void {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    this.selectedGenre.next(selectedValue);
+  }
+
+  
   goToDetails(id: string) {
     this.router.navigate(['/movies', id]);
   }
